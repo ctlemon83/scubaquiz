ded0541437266d621064e37775fcd80c19a97639
Colin Lemon <ctlemon83@gmail.com>
2019-07-19 15:21:04 -0500

    Added more detailed comments

--- highscores.js
+++ highscores.js
@@ -1,6 +1,7 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores  = JSON.parse(localStorage.getItem("highScores")) || [];
 
+// This function dictates how the high scores will be displayed on the "High Scores" page
 highScoresList.innerHTML = 
 	highScores.map(score =>{
 		return `<li class="high-score">${score.name} - ${score.score}</li>`;
