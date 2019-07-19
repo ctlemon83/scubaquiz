ce6c24d070cb22ef4dc84f10028e9ccbe97fcbc8
Colin Lemon <ctlemon83@gmail.com>
2019-07-19 15:20:32 -0500

    Added more detailed comments

--- end.js
+++ end.js
@@ -5,12 +5,15 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 
+// This dictates where the player's current score is displayed
 finalScore.innerText = mostRecentScore;
 
+// This function will not let a player save their score without entering their user name
 username.addEventListener("keyup", () => {
 	saveScoreBtn.disabled = !username.value;
 });
 
+// This function adds the current score to the "high scores" list, if it is in the top five, and orders all high scores highest to lowest
 saveHighScore = (e) => {
 	e.preventDefault();
 	const score = {
@@ -21,6 +24,7 @@ saveHighScore = (e) => {
 	highScores.sort((a,b) => b.score - a.score)
 	highScores.splice(5);
 
+// This saves the high scores in local storage
 	localStorage.setItem("highScores", JSON.stringify(highScores));
 	window.location.assign("index.html");
 };
